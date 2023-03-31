aec679cdd2b6c73c25d3dbc2167626570f0ff5d3
Jh Do <dojh99@gmail.com>
2023-03-31 17:35:27 +0900

    add: Add FSMessage, FSMessageField class

---
+++ src/main/kotlin/message/FSMessage.kt
@@ -0,0 +1,30 @@
+package message
+
+import java.nio.ByteBuffer
+
+abstract class FSMessage {
+    var mBytebuffer: ByteBuffer? = null
+    var mBytesNums: Int = 0
+
+    fun marshall(): ByteBuffer? {
+        mBytesNums = getByteNums()
+        mBytebuffer = ByteBuffer.allocate(mBytesNums)
+
+        mBytebuffer!!.putInt(mBytesNums)
+        marshallBody()
+
+        return mBytebuffer
+    }
+
+    fun unmarshall(byteBuffer : ByteBuffer){
+        byteBuffer.clear()
+        this.mBytesNums = byteBuffer.getInt()
+        unmarshallBody(byteBuffer)
+    }
+
+    abstract fun marshallBody()
+    abstract fun unmarshallBody(byteBuffer: ByteBuffer)
+    protected open fun getByteNums(): Int {
+        return Int.SIZE_BYTES
+    }
+}
