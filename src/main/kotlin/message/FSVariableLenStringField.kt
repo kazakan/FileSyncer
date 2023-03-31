fe3f972bdd326bfb642d43f04c81804ec7fdbc7d
Jh Do <dojh99@gmail.com>
2023-03-31 17:36:34 +0900

    add: Add FSVariableLenStringField

---
+++ src/main/kotlin/message/FSVariableLenStringField.kt
@@ -0,0 +1,35 @@
+package message
+
+import java.nio.ByteBuffer
+
+class FSVariableLenStringField() : FSMessageField {
+
+    var size : Int = 0
+    var str : String = ""
+
+    constructor(str : String) : this(){
+        this.str = str
+        size = this.str.toByteArray().size
+    }
+
+    constructor(byteBuffer : ByteBuffer) : this(){
+        unmarshall(byteBuffer)
+    }
+
+    override fun marshall(byteBuffer: ByteBuffer) {
+        val byteArr = str.toByteArray()
+        size = byteArr.size
+        byteBuffer.putInt(size)
+        byteBuffer.put(byteArr)
+    }
+
+    override fun unmarshall(byteBuffer: ByteBuffer) {
+        size = byteBuffer.getInt()
+        var byteArr = ByteArray(size)
+        str = byteBuffer.get(byteArr).toString()
+    }
+
+    override fun getByteNums(): Int {
+        return Int.SIZE_BYTES + size
+    }
+}
