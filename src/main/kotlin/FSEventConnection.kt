fd9c834d214d643f64683d27269bb217434bab2d
Jh Do <dojh99@gmail.com>
2023-03-31 17:38:27 +0900

    add: Add FSEventConnection class

---
+++ src/main/kotlin/FSEventConnection.kt
@@ -0,0 +1,41 @@
+import message.FSEventMessage
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.net.Socket
+import java.nio.ByteBuffer
+
+class FSEventConnection(var socket: Socket) {
+
+    var ous: OutputStream = socket.getOutputStream()
+    var dous: DataOutputStream = DataOutputStream(ous)
+    var ios: InputStream = socket.getInputStream()
+    var dios = DataInputStream(ios)
+
+    fun sendMessage(message: FSEventMessage) {
+        val buffer = message.marshall()
+        dous.write(buffer!!.array())
+    }
+
+    fun getMessage(): FSEventMessage {
+        val msg = FSEventMessage()
+
+        val nBytes = dios.readInt()
+
+        val byteBuffer = ByteBuffer.allocate(nBytes)
+        byteBuffer.putInt(nBytes)
+        while(byteBuffer.hasRemaining()) {
+            byteBuffer.put(dios.readByte())
+        }
+
+        msg.unmarshall(byteBuffer)
+        return msg
+    }
+
+    fun close() {
+        dous.flush()
+        dous.close()
+        socket.close()
+    }
+}
