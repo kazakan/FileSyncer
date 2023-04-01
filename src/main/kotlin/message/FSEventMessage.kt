945f2aad1aef7f83cbed15b17472346b0aacf032
Jh Do <dojh99@gmail.com>
2023-04-01 09:11:31 +0900

    feat: add additional eventcodes to FSEventMessage class
    Add Some eventcodes.
    Add LOGIN_GRANTED, LOGIN_REJECTED, DOWNLOAD_REQUEST.
    Remove LOGIN_RESPONSE eventcode.

--- src/main/kotlin/message/FSEventMessage.kt
+++ src/main/kotlin/message/FSEventMessage.kt
@@ -7,20 +7,22 @@ object FMEVENT_TYPE {
     const val ASK_ALIVE: Int = 1 // ACK
     const val ANSWER_ALIVE: Int = 2  // ACK
     const val LOGIN_REQUEST: Int = 3 // request login
-    const val LOGIN_RESPONSE: Int = 4 // response for LOGIN_REQUEST
-    const val LOGOUT: Int = 5 // logout
-    const val BROADCAST_CONNECTED: Int = 6 // broadcast message for notify other user connected
-    const val BROADCAST_DISCONNECTED: Int = 7 // broadcast message for notify other user disconnected
-    const val UPLOAD_DONE: Int = 8 // Done upload
-    const val DOWNLOAD_DONE: Int = 9 // Down download
-    const val UPLOAD_REQUEST: Int = 10 // request upload
+    const val LOGIN_GRANTED: Int = 4 // response for LOGIN_REQUEST
+    const val LOGIN_REJECTED: Int = 5
+    const val LOGOUT: Int = 6 // logout
+    const val BROADCAST_CONNECTED: Int = 7 // broadcast message for notify other user connected
+    const val BROADCAST_DISCONNECTED: Int = 8 // broadcast message for notify other user disconnected
+    const val UPLOAD_DONE: Int = 9 // Done upload
+    const val DOWNLOAD_DONE: Int = 10 // Down download
+    const val UPLOAD_REQUEST: Int = 11 // request upload
+    const val DOWNLOAD_REQUEST: Int = 12 // request download
 }
 
 class FSEventMessage : FSMessage() {
 
     var mEventcode = FMEVENT_TYPE.NONE
 
-    var userId  = FSVariableLenStringField()
+    var userId = FSVariableLenStringField()
     var userPassword = FSVariableLenStringField()
 
 
@@ -46,7 +48,11 @@ class FSEventMessage : FSMessage() {
                 userPassword.marshall(mBytebuffer!!)
             }
 
-            FMEVENT_TYPE.LOGIN_RESPONSE -> {
+            FMEVENT_TYPE.LOGIN_GRANTED -> {
+
+            }
+
+            FMEVENT_TYPE.LOGIN_REJECTED -> {
 
             }
 
@@ -66,7 +72,11 @@ class FSEventMessage : FSMessage() {
 
             }
 
-            FMEVENT_TYPE.UPLOAD_REQUEST ->{
+            FMEVENT_TYPE.UPLOAD_REQUEST -> {
+
+            }
+
+            FMEVENT_TYPE.DOWNLOAD_REQUEST -> {
 
             }
 
@@ -98,7 +108,11 @@ class FSEventMessage : FSMessage() {
                 userPassword.unmarshall(byteBuffer)
             }
 
-            FMEVENT_TYPE.LOGIN_RESPONSE -> {
+            FMEVENT_TYPE.LOGIN_GRANTED -> {
+
+            }
+
+            FMEVENT_TYPE.LOGIN_REJECTED -> {
 
             }
 
@@ -118,7 +132,11 @@ class FSEventMessage : FSMessage() {
 
             }
 
-            FMEVENT_TYPE.UPLOAD_REQUEST ->{
+            FMEVENT_TYPE.UPLOAD_REQUEST -> {
+
+            }
+
+            FMEVENT_TYPE.DOWNLOAD_REQUEST -> {
 
             }
 
@@ -132,7 +150,58 @@ class FSEventMessage : FSMessage() {
     override fun getByteNums(): Int {
         var ret = super.getByteNums()
         ret += Int.SIZE_BYTES // mEventCode
-        // ret += 40
+
+        when(mEventcode) {
+            FMEVENT_TYPE.NONE -> {
+                // do nothing
+            }
+
+            FMEVENT_TYPE.ASK_ALIVE -> {
+
+            }
+
+            FMEVENT_TYPE.ANSWER_ALIVE -> {
+
+            }
+
+            FMEVENT_TYPE.LOGIN_REQUEST -> {
+                ret += userId.getByteNums()
+                ret += userPassword.getByteNums()
+            }
+
+            FMEVENT_TYPE.LOGIN_GRANTED -> {
+
+            }
+
+            FMEVENT_TYPE.LOGIN_REJECTED -> {
+
+            }
+
+            FMEVENT_TYPE.LOGOUT -> {
+
+            }
+
+            FMEVENT_TYPE.BROADCAST_CONNECTED, FMEVENT_TYPE.BROADCAST_DISCONNECTED -> {
+                ret += userId.getByteNums()
+            }
+
+            FMEVENT_TYPE.UPLOAD_DONE -> {
+
+            }
+
+            FMEVENT_TYPE.DOWNLOAD_DONE -> {
+
+            }
+
+            FMEVENT_TYPE.UPLOAD_REQUEST -> {
+
+            }
+
+            FMEVENT_TYPE.DOWNLOAD_REQUEST -> {
+
+            }
+        }
+
         return ret
     }
 }
