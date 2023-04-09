f09a17677972b124faed0067550ca43782be57be
Jh Do <dojh99@gmail.com>
2023-04-10 01:32:50 +0900

    chore: format code

--- src/test/kotlin/FSEventConnectionTest.kt
+++ src/test/kotlin/FSEventConnectionTest.kt
@@ -1,64 +1,64 @@
+import java.net.ServerSocket
+import java.net.Socket
 import message.FSEventMessage
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.net.ServerSocket
-import java.net.Socket
 
 class FSEventConnectionTest {
-    @Test
-    fun testSingleMessageSendAndReceive() {
-        var runClient = Runnable {
-            var client = Socket("localhost", 5000)
-            var clientConn = FSEventConnection(client)
-            var messageToSend = FSEventMessage()
-            messageToSend.mEventcode = 77
-            clientConn.sendMessage(messageToSend)
-            client.close()
-        }
-
-        var server = ServerSocket(5000)
-        Thread(runClient).start()
-        var ss = server.accept()
-        var serverConn = FSEventConnection(ss)
-        var receivedMessage = serverConn.getMessage()
-
-        assertEquals(77, receivedMessage.mEventcode)
-
-        ss.close()
-        server.close()
+  @Test
+  fun testSingleMessageSendAndReceive() {
+    var runClient = Runnable {
+      var client = Socket("localhost", 5000)
+      var clientConn = FSEventConnection(client)
+      var messageToSend = FSEventMessage()
+      messageToSend.mEventcode = 77
+      clientConn.sendMessage(messageToSend)
+      client.close()
     }
 
-    @Test
-    fun testTwoMessageSendAndReceive() {
-        val CODE1 = 111
-        val CODE2 = 222
-        val PORT = 5000
+    var server = ServerSocket(5000)
+    Thread(runClient).start()
+    var ss = server.accept()
+    var serverConn = FSEventConnection(ss)
+    var receivedMessage = serverConn.getMessage()
 
-        var runClient = Runnable {
-            var client = Socket("localhost", PORT)
-            var clientConn = FSEventConnection(client)
+    assertEquals(77, receivedMessage.mEventcode)
 
-            var msg = FSEventMessage()
-            msg.mEventcode = CODE1
-            clientConn.sendMessage(msg)
-            msg.mEventcode = CODE2
-            clientConn.sendMessage(msg)
+    ss.close()
+    server.close()
+  }
 
-            client.close()
-        }
+  @Test
+  fun testTwoMessageSendAndReceive() {
+    val CODE1 = 111
+    val CODE2 = 222
+    val PORT = 5000
 
-        var server = ServerSocket(PORT)
-        Thread(runClient).start()
-        var ss = server.accept()
+    var runClient = Runnable {
+      var client = Socket("localhost", PORT)
+      var clientConn = FSEventConnection(client)
 
-        var serverConn = FSEventConnection(ss)
-        var msg1 = serverConn.getMessage()
-        var msg2 = serverConn.getMessage()
+      var msg = FSEventMessage()
+      msg.mEventcode = CODE1
+      clientConn.sendMessage(msg)
+      msg.mEventcode = CODE2
+      clientConn.sendMessage(msg)
 
-        assertEquals(CODE1, msg1.mEventcode)
-        assertEquals(CODE2, msg2.mEventcode)
-
-        ss.close()
-        server.close()
+      client.close()
     }
+
+    var server = ServerSocket(PORT)
+    Thread(runClient).start()
+    var ss = server.accept()
+
+    var serverConn = FSEventConnection(ss)
+    var msg1 = serverConn.getMessage()
+    var msg2 = serverConn.getMessage()
+
+    assertEquals(CODE1, msg1.mEventcode)
+    assertEquals(CODE2, msg2.mEventcode)
+
+    ss.close()
+    server.close()
+  }
 }
