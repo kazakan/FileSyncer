a70b7e1536ddeebf924806c0f8d11d6cdd49c59c
Jh Do <dojh99@gmail.com>
2023-04-10 01:28:05 +0900

    fix: Fix bug making string of object info in unmarshall

--- src/main/kotlin/message/FSVariableLenStringField.kt
+++ src/main/kotlin/message/FSVariableLenStringField.kt
@@ -4,32 +4,33 @@ import java.nio.ByteBuffer
 
 class FSVariableLenStringField() : FSMessageField {
 
-    var size : Int = 0
-    var str : String = ""
+  var size: Int = 0
+  var str: String = ""
 
-    constructor(str : String) : this(){
-        this.str = str
-        size = this.str.toByteArray().size
-    }
+  constructor(str: String) : this() {
+    this.str = str
+    size = this.str.toByteArray().size
+  }
 
-    constructor(byteBuffer : ByteBuffer) : this(){
-        unmarshall(byteBuffer)
-    }
+  constructor(byteBuffer: ByteBuffer) : this() {
+    unmarshall(byteBuffer)
+  }
 
-    override fun marshall(byteBuffer: ByteBuffer) {
-        val byteArr = str.toByteArray()
-        size = byteArr.size
-        byteBuffer.putInt(size)
-        byteBuffer.put(byteArr)
-    }
+  override fun marshall(byteBuffer: ByteBuffer) {
+    val byteArr = str.toByteArray()
+    size = byteArr.size
+    byteBuffer.putInt(size)
+    byteBuffer.put(byteArr)
+  }
 
-    override fun unmarshall(byteBuffer: ByteBuffer) {
-        size = byteBuffer.getInt()
-        var byteArr = ByteArray(size)
-        str = byteBuffer.get(byteArr).toString()
-    }
+  override fun unmarshall(byteBuffer: ByteBuffer) {
+    size = byteBuffer.getInt()
+    var byteArr = ByteArray(size)
+    byteBuffer.get(byteArr)
+    str = String(byteArr)
+  }
 
-    override fun getByteNums(): Int {
-        return Int.SIZE_BYTES + size
-    }
+  override fun getByteNums(): Int {
+    return Int.SIZE_BYTES + size
+  }
 }
