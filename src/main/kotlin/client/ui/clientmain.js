f5aa0015a6f9e4b94457d01235c87951a7b1f7a8
Jh Do <dojh99@gmail.com>
2023-04-11 10:36:51 +0900

    add: add client main working front page

---
+++ src/main/kotlin/client/ui/clientmain.js
@@ -0,0 +1,38 @@
+function uploadFile(path) {}
+
+function downloadFile(path) {}
+
+function getFileList(dir) {
+  var params = new URLSearchParams();
+  params.append("dir", dir);
+  axios.get("/api/showFolder", { params }).then(
+    (response) => {
+      var element = document.getElementById("file-list");
+      var result = response.data;
+
+      console.log(result[0]);
+
+      element.innerHTML = "";
+
+      if (result.length > 0) {
+        for (var i = 0; i < result.length; ++i) {
+          var row =
+            '<div class="row">' +
+            result[i]["name"] +
+            '<button style="float:right;">download</button>' +
+            '<button style="float:right;">upload</button>' +
+            '<span style="float:right; margin-right:5px;">' +
+            result[i]["status"] +
+            "</span>" +
+            "</div> \n";
+          element.innerHTML += row;
+        }
+      } else {
+        element.innerHTML += "<div>No files to show</div>";
+      }
+    },
+    (error) => {}
+  );
+}
+
+function disconnect() {}
