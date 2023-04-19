29ad10b1e7f661644f3ffc19c9615bbb8e65fd6e
Jh Do <dojh99@gmail.com>
2023-04-19 15:39:43 +0900

    chore: format code

--- src/main/kotlin/client/ui/clientmain.js
+++ src/main/kotlin/client/ui/clientmain.js
@@ -1,91 +1,92 @@
 function uploadFile(path) {
-  var params = new URLSearchParams();
-  params.append("fname", path);
+    var params = new URLSearchParams();
+    params.append("fname", path);
 
-  axios.post("/api/uploadFile", params).then(
-    (response) => {
-      getFileList("");
-    },
-    (error) => {
-      alert(error);
-    }
-  );
+    axios.post("/api/uploadFile", params).then(
+        (response) => {
+            getFileList("");
+        },
+        (error) => {
+            alert(error);
+        }
+    );
 }
 
 function downloadFile(path) {}
 
 function getFileList(dir) {
-  var params = new URLSearchParams();
-  params.append("dir", dir);
-  axios.get("/api/showFolder", { params }).then(
-    (response) => {
-      var element = document.getElementById("file-list");
-      var result = response.data;
+    var params = new URLSearchParams();
+    params.append("dir", dir);
+    axios.get("/api/showFolder", { params }).then(
+        (response) => {
+            var element = document.getElementById("file-list");
+            var result = response.data;
 
-      console.log(result[0]);
+            console.log(result[0]);
 
-      element.innerHTML = "";
+            element.innerHTML = "";
 
-      if (result.length > 0) {
-        for (var i = 0; i < result.length; ++i) {
-          var isLocal =
-            result[i]["status"] == "Both" ||
-            result[i]["status"] == "Local Only";
-          var isCloud =
-            result[i]["status"] == "Both" ||
-            result[i]["status"] == "Cloud Only";
+            if (result.length > 0) {
+                for (var i = 0; i < result.length; ++i) {
+                    var isLocal =
+                        result[i]["status"] == "Both" ||
+                        result[i]["status"] == "Local Only";
+                    var isCloud =
+                        result[i]["status"] == "Both" ||
+                        result[i]["status"] == "Cloud Only";
 
-          var row = '<div class="row">' + result[i]["name"];
+                    var row = '<div class="row">' + result[i]["name"];
 
-          // download button
-          row += '<button style="float:right;" disabled="">download</button>';
+                    // download button
+                    row +=
+                        '<button style="float:right;" disabled="">download</button>';
 
-          // upload button
-          row +=
-            "<button onclick=\"uploadFile('" +
-            result[i]["name"] +
-            '\')" style="float:right;" ';
-          if (!isLocal) row += 'disabled=""';
-          row += ">upload</button>";
-          row +=
-            '<span style="float:right; margin-right:5px;">' +
-            result[i]["status"] +
-            "</span>";
+                    // upload button
+                    row +=
+                        "<button onclick=\"uploadFile('" +
+                        result[i]["name"] +
+                        '\')" style="float:right;" ';
+                    if (!isLocal) row += 'disabled=""';
+                    row += ">upload</button>";
+                    row +=
+                        '<span style="float:right; margin-right:5px;">' +
+                        result[i]["status"] +
+                        "</span>";
 
-          row += "</div> \n";
-          element.innerHTML += row;
-        }
-      } else {
-        element.innerHTML += "<div>No files to show</div>";
-      }
-    },
-    (error) => {}
-  );
+                    row += "</div> \n";
+                    element.innerHTML += row;
+                }
+            } else {
+                element.innerHTML += "<div>No files to show</div>";
+            }
+        },
+        (error) => {}
+    );
 }
 
 function disconnect() {
-  axios.post("/api/disconnect", {}).then(
-    (response) => {
-      alert("Disconnected successfully.");
-      window.location = "/fs/login";
-    },
-    (error) => {
-      alert("Something went wrong during disconnecting TT");
-    }
-  );
+    axios.post("/api/disconnect", {}).then(
+        (response) => {
+            alert("Disconnected successfully.");
+            window.location = "/fs/login";
+        },
+        (error) => {
+            alert("Something went wrong during disconnecting TT");
+        }
+    );
 }
 
 function addReportMessage(msg) {
-  var messageWindow = document.getElementById("right-panel");
-  messageWindow.innerHTML += "<p>" + msg + "</p>";
-  messageWindow.scrollTo(0, messageWindow.scrollHeight);
+    var messageWindow = document.getElementById("right-panel");
+    messageWindow.innerHTML += "<p>" + msg + "</p>";
+    messageWindow.scrollTo(0, messageWindow.scrollHeight);
 }
 
 function onLoad() {
-  getFileList("/");
-  const eventSource = new EventSource("/api/msgSse");
+    getFileList("/");
+    const eventSource = new EventSource("/api/msgSse");
 
-  eventSource.addEventListener("message", (event) => {
-    addReportMessage(event.data);
-  });
+    eventSource.addEventListener("message", (event) => {
+        addReportMessage(event.data);
+    });
 }
