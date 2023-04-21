458bac11d011ad463ddb723fa8405c41ad614ae0
Jh Do <dojh99@gmail.com>
2023-04-21 18:08:19 +0900

    add: Add ArgParser

---
+++ common/src/test/kotlin/filesyncer/common/argparse/ArgParserTest.kt
@@ -0,0 +1,22 @@
+package filesyncer.common.argparse
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ArgParserTest {
+
+    @Test
+    fun testParser() {
+        var parser = ArgParser()
+        parser.addArg("apos", ArgType.STRING, false)
+        parser.addArg("bpos", ArgType.STRING, false)
+        parser.addArg("aopt", ArgType.STRING, true, "hahaha")
+        parser.addArg("bopt", ArgType.INT, true, 50)
+
+        val args = parser.parse(listOf("aaa", "-a", "kelvin", "-b", "5050", "aaa"))
+        Assertions.assertEquals("aaa", args["apos"])
+        Assertions.assertEquals("aaa", args["bpos"])
+        Assertions.assertEquals("kelvin", args["aopt"])
+        Assertions.assertEquals(5050, args["bopt"])
+    }
+}
