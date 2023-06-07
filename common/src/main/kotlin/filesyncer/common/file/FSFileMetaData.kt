5261427e186e6ebce40ee15edd023f3228db7e70
Jh Do <dojh99@gmail.com>
2023-06-07 14:12:31 +0900

    add: Add conversion from array

--- common/src/main/kotlin/filesyncer/common/file/FSFileMetaData.kt
+++ common/src/main/kotlin/filesyncer/common/file/FSFileMetaData.kt
@@ -10,4 +10,19 @@ data class FSFileMetaData(
     var path: File? = null,
     var owner: String = "",
     var shared: List<String> = emptyList()
-) {}
+) {
+    fun toStringArray(): Array<String> {
+        return arrayOf(name, "$fileSize", "$timeStamp", md5, owner, shared.joinToString("/"))
+    }
+
+    fun fromStringArray(arr: Array<String>) {
+        if (arr.size < 6) throw error("arr is shorter than 6.")
+
+        name = arr[0]
+        fileSize = arr[1].toLong()
+        timeStamp = arr[2].toLong()
+        md5 = arr[3]
+        owner = arr[4]
+        shared = arr[5].split("/")
+    }
+}
